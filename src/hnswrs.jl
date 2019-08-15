e8eb96da900a6229de8c81b2abfdab511b272711
jean-pierre both <jeanpierre.both@gmail.com>
2019-08-15 20:49:03 +0200

    call to parallel searched entered

--- src/hnswrs.jl
+++ src/hnswrs.jl
@@ -131,15 +131,15 @@ end
 
 # we must return a Vector{Vector{Neighbour}} , one Vector{Neighbour} per request input
 
-function parallel_search_f32(ptr::Ref{HnswApi}, datas::Vector{Vector{Float32}}, knbn::UInt64, ef_search:: UInt64)
+function parallel_search_f32(ptr::Ref{HnswApi}, datas::Vector{Vector{Float32}}, knbn::Int64, ef_search:: Int64)
     nb_vec = length(datas)
     len = length(datas[1])
     # make a Vector{Ref{Float32}} where each ptr is a ref to datas[i] memory beginning
-    vec_ref = map(x-> Ref(x), datas)
+    vec_ref = map(x-> pointer(x), datas)
     neighbourhood_vec_ptr = ccall(
         (:parallel_search_neighbours_f32, libhnswso),
         Ptr{NeighbourhoodVect},
-        (Ref{HnswApi}, UInt, Ref{Float32}, UInt, UInt),
+        (Ref{HnswApi}, UInt, UInt, Ref{Ptr{Float32}}, UInt, UInt),
         ptr, UInt(nb_vec), UInt(len), vec_ref, UInt(knbn), UInt(ef_search)
     )
     @debug "\n parallel_search_neighbours_f32 returned pointer" neighbourhood_vec_ptr
