b3f0b2508122ee4fa17549e7584a51a367a1f4b2
jean-pierre both <jeanpierre.both@gmail.com>
2019-08-25 18:21:05 +0200

    functions createHnswApi for function pointer + doc

--- src/Hnsw.jl
+++ src/Hnsw.jl
@@ -9,6 +9,7 @@ export
     Neighbour,
     Neighbourhood,
     HnswApi,
+    createHnswApi,
     setRustlibPath,
     insert,
     search
@@ -24,12 +25,21 @@ This is the structure encapsulting the rust api.
  -------
 
 * rust : a reference pointer coming from rust library. An opaque structure not to be manipulated.
+
 * type : The type of Vector
+
 * maxNbConn : The number of connection to use in Hnsw structure. Between 8 and 64.
+
 * efConstruction : The with of neighbours search used when constructing links between nodes.
     A rule of thumb is between maxNbConn and 64.
+
 * distname : the name o the distance to use. It can be "DistL1", "DistL2", "DistCosine", DistDot", 
         "DistHamming", "DistJaccard"
+
+* A pointer to a function computing distances created by the macro @cfunction.
+    The signature of the function must be Cfloat, (Ptr{Cfloat}, Ptr{Cfloat}, Culonglong))
+    See the test testdistptr in test.jl and the documentation of @cfunction.
+
 """
 mutable struct HnswApi
     rust :: Ref{Hnswrs}
@@ -37,19 +47,47 @@ mutable struct HnswApi
     maxNbConn::Int64
     efConstruction::Int64
     distname::String
-    #
-    function Hnsw(type::DataType, maxNbConn::Int64, efConstruction::Int64, distname::String)
-        rust_type_name = checkForImplementedType(type)
-        rust = hnswInit(type, maxNbConn, efConstruction, distname)
-        new(rust, type, maxNbConn, efConstruction, distname)
-    end
+    distfunctPtr :: Some{Ptr{Cvoid}}
 end
+
+
+"""
+  #  `function createHnswApi(type::DataType, maxNbConn::Int64, efConstruction::Int64, distname::String)`
+
+  This function creates a standard HnswApi with pre-coded distances. 
+  It benefits from parallel insert/search method
+    
+"""
+function createHnswApi(type::DataType, maxNbConn::Int64, efConstruction::Int64, distname::String)
+    rust_type_name = checkForImplementedType(type)
+    rust = hnswInit(type, maxNbConn, efConstruction, distname)
+    HnswApi(rust, type, maxNbConn, efConstruction, distname, Nothing)
+end
+
+"""
+# ` function createHnswApi(type::DataType, maxNbConn::Int64, efConstruction::Int64, distance::Ptr{Cvoid}) `
+
+    Create a HnswApi with a custom distance function pointer. In this case the name of the distance 
+    is set to "CustomPtr"
+    * CAUTION:
+    It must be noted that as Julia (for version < 1.3) is not thread safe the api cannot use 
+    parallel insertion/search in this case.
+
+"""
+function createHnswApi(type::DataType, maxNbConn::Int64, efConstruction::Int64, distance::Ptr{Cvoid})
+    rust_type_name = checkForImplementedType(type)
+    rust = hnswInit(type, maxNbConn, distance)
+    # we store distance to avoid garbage collection of pointer as it is referenced in rust library.
+    HnswApi(rust, type, maxNbConn, efConstruction, "CustomPtr", Some(distance))
+end
+
+
 
 
 """
  #  insert 
 
- function `insert(hnsw::HnswApi, data::Vector{T}, id::UInt64) where {T <: Number}`
+ `function insert(hnsw::HnswApi, data::Vector{T}, id::UInt64) where {T <: Number}`
 
 ## Args
   
@@ -65,7 +103,7 @@ end
 """
 # parallel insertion
 
-function `insert(hnsw::HnswApi, datas::Vector{Tuple{Vector{T}, UInt}}) where {T <: Number}`
+`function insert(hnsw::HnswApi, datas::Vector{Tuple{Vector{T}, UInt}}) where {T <: Number}`
 
 This function does a block parallel insertion of datas in structure.
 
@@ -81,7 +119,7 @@ end
 """
 # search
 
-function `search(hnsw::Hnsw, vector::Vector{T}, knbn::Int64, ef_search ::Int64) where {T<:Number}`
+` function search(hnsw::Hnsw, vector::Vector{T}, knbn::Int64, ef_search ::Int64) where {T<:Number}`
 
  ## Args
  
--- test/test.jl
+++ test/test.jl
@@ -28,7 +28,7 @@ end
 #  rust calling so length will be passes as a usize.
 
 function mydist(pa::Ptr{Float32}, pb::Ptr{Float32}, l::UInt64)
-    println("in mydist")
+#    println("in mydist")
     #
    dist = 0
     #   
@@ -48,17 +48,23 @@ function testdistptr()
     dim = 10
     hnsw = hnswInit(Float32, 8, 16, mydist_ptr)
     # block // insertion
-    datas = rand(Float32, (dim, 50));
-    data_insert = map(i -> ( rand(Float32, dim) , UInt(i) ), 1:size(datas)[2])
-    parallel_insert(hnsw, data_insert) 
+    nbinsert = 100
+    for i in 1:nbinsert
+        data = rand(Float32, dim);
+        one_insert(hnsw, data, UInt64(i))
+    end
     # testing search
+    println("testing one search")
     v1 = rand(Float32, dim)
     neighbours = one_search(hnsw, v1, 10, 16)
     # testing block // search
+    println("testing parallel search")
     datas = map(i -> rand(Float32, dim), 1:100)
     neighbours = parallel_search(hnsw, datas, 10, 16)
     true
 end
+
+
 
 @testset "f32" begin
     @test testdistl1()
