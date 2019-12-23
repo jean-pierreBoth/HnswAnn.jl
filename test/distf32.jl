86ddeb923ebce769c24aed4f5a1c6a5f8223d094
jean-pierre both <jeanpierre.both@gmail.com>
2019-12-23 11:25:48 +0100

    add file runtests.jl

--- test/distf32.jl
+++ test/distf32.jl
@@ -1,16 +1,3 @@
-using Hnsw
-using Test
-
-include("../src/hnswrs.jl")
-
-ldpath = "/home/jpboth/Rust/hnswlib-rs/target/debug/"
-setRustlibPath(ldpath)
-
-using Logging
-using Base.CoreLogging
-
-logger = ConsoleLogger(stdout, CoreLogging.Debug)
-global_logger(logger)
 
 function testdistl1()
     dim = 10
@@ -67,43 +54,4 @@ function testdistptr()
     v1 = rand(Float32, dim)
     neighbours = search(hnsw, v1, 10, 16)
     true
-end
-
-
-function testdump()
-    dim = 10
-    hnsw = createHnswApi(Float32, 8, 16, "DistL1")
-    # block // insertion
-    nbinsert = 500
-    parallel = true
-    if parallel 
-        datas = rand(Float32, (dim, nbinsert));
-        data_insert = map(i -> ( rand(Float32, dim) , UInt(i) ), 1:size(datas)[2])
-        insert(hnsw, data_insert)
-    else
-        for i in 1:nbinsert
-            data = rand(Float32, dim);
-            insert(hnsw, data, UInt64(i))
-        end       
-    end
-    #
-    res = fileDump(hnsw, "testdumpfromjulia")
-    if res > 0
-        true
-    else
-        @warn "file dump failed : " "testdumpfromjulia"
-        false
-    end
-end
-
-
-function testreload()
-    hnsw2 = loadHnsw("testdumpfromjulia", Float32, "DistL1")
-end
-
-
-
-@testset "f32" begin
-    @test testdistl1()
-    @test testdistptr()
 end
